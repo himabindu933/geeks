620e1936081e163c72dc9a88a40158bd6ce8db6f
himabindu933 <himabindu@savart.com>
2023-08-01 16:44:47 +0530

    sample menu added

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,13 @@
+"use strict";
+
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <div>
+      <Link href="/">home</Link>
+      <Link href="/testimonials">test</Link>
+      <Link href="/about">about</Link>
+    </div>
+  );
+}
---
+++ src/app/testimonials/page.tsx
@@ -0,0 +1,5 @@
+"use strict";
+
+export default function Testimonials() {
+  return <div>Testimonials</div>;
+}
