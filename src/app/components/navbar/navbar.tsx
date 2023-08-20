db6a873669fdfedafc19db948af71c9bd8060190
Mohammed Khalid <khalid.26227@gmail.com>
2023-08-20 10:21:55 +0530

    Navbar Menuitems dropdown completed

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -15,19 +15,83 @@ export default function Navbar() {
       <div>
         <SearchBar />
       </div>
-      <div>
-        <Link href="/categories">Categories</Link>
-        <Link href="/business">Geeks Business</Link>
-        <Link href="/mentors">Geeks Mentors</Link>
-      </div>
-      <div>
-        <AiOutlineShoppingCart />
+      <div className="navbar__pages">
+        <div className="dropdown">
+          <button className="dropbtn">Categories</button>
+          <div className="dropdown-content">
+            <div className="submenu">
+              <a href="#">Visual Arts</a>
+              <div className="submenu-content">
+                <a href="#">2D</a>
+                <a href="#">Visual Effects</a>
+                <a href="#">3D</a>
+                <a href="#">Gaming</a>
+                <a href="#">Visualisation</a>
+                <a href="#">Editing</a>
+              </div>
+            </div>
+            <div className="submenu">
+              <a href="#">Fine Arts</a>
+              <div className="submenu-content">
+                <a href="#">Drawing</a>
+                <a href="#">Painting</a>
+                <a href="#">Concept Arts</a>
+              </div>
+            </div>
+            <a href="#">Applied Arts</a>
+          </div>
+        </div>
+        <Link className="nav-bar-item" href="/business">
+          Geeks Business
+        </Link>
+        <Link className="nav-bar-item" href="/mentors">
+          Geeks Mentors
+        </Link>
       </div>
 
-      <div>
+      <div className="navbar__buttons">
+        <div>
+          <AiOutlineShoppingCart size={30} style={{ cursor: "pointer" }} />
+        </div>
         <button>Sign Up</button>
         <button>Log In</button>
       </div>
     </nav>
   );
+  // return (
+  //   <nav className="navbar">
+  //     <div>
+  //       <h1>
+  //         <Link href="/">GEEKS</Link>
+  //       </h1>
+  //     </div>
+  //     <div>
+  //       <SearchBar />
+  //     </div>
+  //     <div className="navbar__pages">
+  //       <div className="dropdown">
+  //         <Link className="dropbtn" href="/categories">
+  //           Categories
+  //         </Link>
+
+  //         {/* <button className="dropbtn"></button> */}
+  //         <div className="dropdown-content">
+  //           <a href="#">Link 1</a>
+  //           <a href="#">Link 2</a>
+  //           <a href="#">Link 3</a>
+  //         </div>
+  //       </div>
+  //       <Link href="/business">Geeks Business</Link>
+  //       <Link href="/mentors">Geeks Mentors</Link>
+  //     </div>
+
+  //     <div className="navbar__buttons">
+  //       <div>
+  //         <AiOutlineShoppingCart size={30} style={{ cursor: "pointer" }} />
+  //       </div>
+  //       <button>Sign Up</button>
+  //       <button>Log In</button>
+  //     </div>
+  //   </nav>
+  // );
 }
--- src/app/components/searchbar/SearchBar.tsx
+++ src/app/components/searchbar/SearchBar.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from "react";
+import { AiOutlineSearch } from "react-icons/Ai";
 
 const SearchBar = () => {
   return (
     <>
-    <input
-    placeholder='search here'
-    />
+      <div className="searchBar">
+        <input placeholder="search here" />
+        <AiOutlineSearch size={35} className="searchBar__icon" />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
